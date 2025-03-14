70515ed0ea1c2577e5b4b7176c371bcbe8725ba9
rupesh-ranjan <rupesh.ranjan@outlook.in>
2025-03-14 20:49:06 +0530

    Featured courses added

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import React from "react";
-import { Spotlight } from "./spotlight-new";
+import { Spotlight } from "./ui/spotlight-new";
+import { Button } from "./ui/moving-border";
 
 function HeroSection() {
   return (
-    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+    <div className="h-auto md:h-[36rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       <Spotlight />
       <div className="p-4 realtive z-10 w-full text-center">
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
@@ -16,7 +17,14 @@ function HeroSection() {
           skills, join us to unlock your true potential.
         </p>
         <div className="mt-4">
-          <Link href={"/courses"}>Explore Courses</Link>
+          <Link href={"/courses"}>
+            <Button
+              borderRadius="1.75rem"
+              className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+            >
+              Explore Courses
+            </Button>
+          </Link>
         </div>
       </div>
     </div>
---
+++ tailwind-plugins.js
@@ -0,0 +1,1 @@
+// This file is used to create custom plugins for Tailwind CSS.
