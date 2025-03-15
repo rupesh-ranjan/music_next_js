258df4a8faa1d10c556ef4b775c04d8ed7e8e148
rupesh-ranjan <rupesh.ranjan@outlook.in>
2025-03-15 20:25:46 +0530

    all courses page added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,14 +20,14 @@ function Navbar({ className }: { className?: string }) {
         </Link>
         <MenuItem setActive={setActive} active={active} item="Our Courses">
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href={"#"}>All Courses</HoveredLink>
-            <HoveredLink href={"#"}>Basic Music Theory</HoveredLink>
-            <HoveredLink href={"#"}>AdvancedComposition</HoveredLink>
-            <HoveredLink href={"#"}>Song Writing</HoveredLink>
-            <HoveredLink href={"#"}>Music Production</HoveredLink>
+            <HoveredLink href={"/courses"}>All Courses</HoveredLink>
+            <HoveredLink href={"/"}>Basic Music Theory</HoveredLink>
+            <HoveredLink href={"/"}>AdvancedComposition</HoveredLink>
+            <HoveredLink href={"/"}>Song Writing</HoveredLink>
+            <HoveredLink href={"/"}>Music Production</HoveredLink>
           </div>
         </MenuItem>
-        <Link href={"/"}>
+        <Link href={"/contact"}>
           <MenuItem
             setActive={setActive}
             active={active}
