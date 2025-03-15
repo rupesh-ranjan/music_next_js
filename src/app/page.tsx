5f32b7958a393e2f2403e8e00f65b29cc7007dc4
rupesh-ranjan <rupesh.ranjan@outlook.in>
2025-03-15 19:10:38 +0530

    footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Instructors from "@/components/Instructors";
 import MusicSchollTestimonialCards from "@/components/TestimonicalCard";
@@ -14,6 +15,7 @@ export default function Home() {
       <MusicSchollTestimonialCards />
       <UpcomingWebinars />
       <Instructors />
+      <Footer />
     </main>
   );
 }
