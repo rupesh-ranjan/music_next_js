74e6baa1b2b7b4d70c7d594629250fc805ef5001
rupesh-ranjan <rupesh.ranjan@outlook.in>
2025-03-16 14:15:20 +0530

    fixed es lint errors

--- src/components/WhyChooseUs.tsx
+++ src/components/WhyChooseUs.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import React from "react";
 import musicData from "@/data/music_content.json";
--- src/components/ui/background-beams-with-collision.tsx
+++ src/components/ui/background-beams-with-collision.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 import { cn } from "@/utils/cn";
 import { motion, AnimatePresence } from "motion/react";
